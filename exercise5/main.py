20ec3fd6400b711fb144431251053dc70d5ddbe2
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2026-03-03 19:20:49 +0100

    Working on task 1

--- exercise5/main.py
+++ exercise5/main.py
@@ -9,53 +9,70 @@ from kwave.kWaveSimulation import SimulationOptions
 from kwave.options.simulation_execution_options import SimulationExecutionOptions
 from kwave.utils.signals import tone_burst
 
-## SIMULATION SETUP
+# SIMULATION SETUP
 c0 = 1540  # [m/s] Speed of sound
 rho0 = 1000  # [kg/m^3] Density of water
 source_f0 = 1e6  # [Hz] Source frequency
 source_amp = 1e6  # [Pa] Source amplitude
-source_cycles = 5  # Number of cycles in the pulse
+source_cycles = 2  # Number of cycles in the pulse
 
-grid_size_x = 40e-3  # [m] Grid size in x (NB: Depth in k-Wave)
+aperture_size = 10e-3
+aperture_index = 10
+grid_size_x = 100e-3  # [m] Grid size in x (NB: Depth in k-Wave)
 grid_size_y = 40e-3  # [m] Grid size in z (NB: Width in k-Wave)
-ppw = 5  # Points per wavelength
+focal_depth = 70e-3  # [m] Focus depth
+ppw = 8  # Points per wavelength
 cfl = 0.3  # Related to the time resolution, lower is more accurate (no need to change)
 
-viz_t = grid_size_y / 2 / c0 * 0.75  # Visualization time [s]
+viz_t = grid_size_y / 2 / c0 * 1.2  # Visualization time [s]
 
-## SETUP SIMULATION GRID AND TIME STEPS
+# SETUP SIMULATION GRID AND TIME STEPS
 dx = c0 / (ppw * source_f0)  # Grid resolution
 Nx = round(grid_size_x / dx)  # Number of grid points in x, NB: This is depth
 Ny = round(grid_size_y / dx)  # Number of grid points in y, NB: This is width (lateral)
+Na = round(Ny * aperture_size // grid_size_y)  # Number of grid points in the aperture
 kgrid = kWaveGrid([Nx, Ny], [dx, dx])
 kgrid.makeTime(
     c0, cfl
 )  # Make time array based on the Courant-Friedrichs-Lewy (CFL) condition
 
-## SETUP SOURCE
+# SETUP SOURCE
 # Source signal, Gaussian pulse
-source_sig = source_amp * tone_burst(1 / kgrid.dt, source_f0, source_cycles)
+signal_offset = np.arange(Na // 2)
+source_sig = source_amp * tone_burst(
+    1 / kgrid.dt,
+    source_f0,
+    source_cycles,
+    signal_offset=signal_offset,
+)
+plt.figure(tight_layout=True)
+plt.title("Source signals")
+for i, sig in enumerate(source_sig):
+    plt.plot(sig, label=f"signal {i}")
+plt.ylabel("Amplitude [Pa]")
+plt.xlabel("Sample")
 
 # Define kWave source object
 source = kSource()
 source.p_mask = np.zeros_like(kgrid.x)
-source.p_mask[round(kgrid.Nx / 2), round(kgrid.Ny / 2)] = (
-    1  # Source position, centered in the grid
-)
+# Start source points at the edge.
+# Approximate half of the aperture outside the grid
+for point in range(0, Na // 2):
+    source.p_mask[aperture_index, point] = 1
 source.p = source_sig  # Source signal (pressure source)
 
-## SETUP SENSOR
+# SETUP SENSOR
 sensor = kSensor(
     record=["p", "p_max"]
 )  # Sensor object, record pressure and maximum pressure
 sensor.mask = np.ones_like(kgrid.x)  # Sensor mask, all grid points
 
-## SETUP MEDIUM
+# SETUP MEDIUM
 medium = kWaveMedium(
     sound_speed=c0, density=rho0
 )  # Define medium object, simple homogeneous medium
 
-## RUN SIMULATION
+# RUN SIMULATION
 simulation_options = SimulationOptions(
     pml_auto=True,
     pml_inside=False,
@@ -79,29 +96,33 @@ p_field = np.reshape(
     sensor_data["p"], (kgrid.Nt, Nx, Ny), order="F"
 )  # Stored in Fortran ordering for some reason
 
-## VISUALIZATION
+# VISUALIZATION
 plt.style.use("dark_background")
 
-# Get frame number to plot
-N_frame = np.round(np.where(kgrid.t_array[0] > viz_t)[0][0]).astype(
-    int
-)  # Number of frames to visualize
+# Create updated coordinate axis
+x_axis = (np.arange(Nx) * dx - aperture_index * dx) * 1e3
+y_axis = kgrid.y_vec * 1e3
 
-# Normalize frames based on the maximum value over all frames
-max_value = np.max(np.abs(p_field))
-p_plot = p_field / max_value
+for alpha in [0.2, 0.5, 0.8, 1.2, 1.5, 1.8]:
+    viz_t = grid_size_x / 2 / c0 * alpha  # Visualization time [s]
+    # Get frame number to plot
+    N_frame = np.round(np.where(kgrid.t_array[0] > viz_t)[0][0]).astype(
+        int
+    )  # Number of frames to visualize
 
-cmap = "twilight"
-extent = (
-    np.array([kgrid.y_vec[0], kgrid.y_vec[-1], kgrid.x_vec[0], kgrid.x_vec[-1]]) * 1e3
-)
+    # Normalize frames based on the maximum value over all frames
+    max_value = np.max(np.abs(p_field))
+    p_plot = p_field / max_value
 
-# Create a figure and axis
-fig, ax = plt.subplots()
-image = plt.imshow(p_plot[N_frame], cmap=cmap, vmin=-0.1, vmax=0.1, extent=extent)
-plt.colorbar()
-plt.xlabel("x [mm]")
-plt.ylabel("z [mm]")
-plt.title("A single point source")
+    cmap = "twilight"
+    extent = [y_axis[0], y_axis[-1], x_axis[-1], x_axis[0]]
+
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(5, 8), constrained_layout=True)
+    image = plt.imshow(p_plot[N_frame], cmap=cmap, vmin=-0.1, vmax=0.1, extent=extent)
+    plt.colorbar()
+    plt.xlabel("x [mm]")
+    plt.ylabel("z [mm]")
+    plt.title(f"{Na} point sources")
+
 plt.show()
-
